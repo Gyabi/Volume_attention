1a90afb5d2704c9289c311d0e5b8dc420b137b22
yuichikano <58720709+yuichikano@users.noreply.github.com>
2021-02-27 03:11:07 +0900

    second commit

--- Volume_attention.py
+++ Volume_attention.py
@@ -1,11 +1,35 @@
 import cv2
+from playsound import playsound
+from screeninfo import get_monitors
+import threading
 
-img_path = "img/icon.jpg"
+param = {
+    "img_path":"img/icon.jpg",
+    "use_moniter":1,
+    "voice_path":"voice/alert.mp3"
+}
 
-img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
-# cv2.namedWindow('attention', cv2.WINDOW_KEEPRATIO)
-# cv2.setWindowProperty('attention', cv2.WND_PROP_ASPECT_RATIO, cv2.WINDOW_KEEPRATIO)
-cv2.imshow("attention", img)
-# cv2.waitKey(2000)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# 画像を表示する関数
+def image_show(param):
+    img = cv2.imread(param["img_path"])
+    for m in get_monitors():
+        if m.name == "\\\\.\\DISPLAY" + str(param["use_moniter"]):
+            moniter_height = m.height
+            moniter_width = m.width
+    img = cv2.resize(img, (int(img.shape[0]*1.5),int(img.shape[1]*1.5)))
+    cv2.imshow("attention", img)
+    cv2.moveWindow("attention", int(moniter_width/2-img.shape[0]/2), int(moniter_height/2-img.shape[1]/2))
+    cv2.waitKey(2500)
+    cv2.destroyAllWindows()
+
+# 音声を出力する関数
+def voice_out(param):
+    playsound(param["voice_path"])
+
+thread1 = threading.Thread(target=image_show, args=(param, ))
+thread2 = threading.Thread(target=voice_out, args=(param, ))
+thread1.start()
+thread2.start()
+thread1.join()
+thread2.join()
+
--- rgb2rgba.py
+++ rgb2rgba.py
@@ -1,1 +1,22 @@
 import cv2
+import numpy as np
+
+rgb_img_path = "img/icon_rgb.jpg"
+min_main_color = np.array([1,200,200])
+max_main_color = np.array([100,255,255])
+rgb_img = cv2.imread(rgb_img_path)
+
+a_data = np.ones((rgb_img.shape[0],rgb_img.shape[1]))
+mask = np.ones((rgb_img.shape[0],rgb_img.shape[1]))
+
+# 指定した色相の範囲内の要素を1に変更
+for i,a in enumerate(rgb_img):
+    for j,b in enumerate(a):
+        if ((min_main_color <= b).all() and (max_main_color >= b).all()):
+            mask[i,j] = 0
+
+
+
+cv2.imshow("attention", mask)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
